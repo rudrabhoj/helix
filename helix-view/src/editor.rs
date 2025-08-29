ddd258d85daaaf305fa934225c7f9a82273cf69e
JJ <git@toki.la>
2025-08-29 14:15:57 -0700

    Refactor rainbow indent guides configuration into being a boolean on/off

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -1007,20 +1007,12 @@ impl Default for WhitespaceCharacters {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-pub enum RainbowIndent {
-    None,
-    Dim,
-    Normal,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(default, rename_all = "kebab-case")]
 pub struct IndentGuidesConfig {
     pub render: bool,
     pub character: char,
     pub skip_levels: u8,
-    pub rainbow_indent: RainbowIndent,
+    pub rainbow: bool,
 }
 
 impl Default for IndentGuidesConfig {
@@ -1029,7 +1021,7 @@ impl Default for IndentGuidesConfig {
             skip_levels: 0,
             render: false,
             character: '│',
-            rainbow_indent: RainbowIndent::None,
+            rainbow: false,
         }
     }
 }
